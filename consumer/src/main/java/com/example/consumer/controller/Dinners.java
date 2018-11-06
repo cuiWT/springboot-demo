16b8ba9be774267ddb22a3399b1664c0b33b34c1
cui.wt <cui.wt@belle.com.cn>
2018-11-06 15:31:39 +0800

    remove swagger

--- consumer/src/main/java/com/example/consumer/controller/Dinners.java
+++ consumer/src/main/java/com/example/consumer/controller/Dinners.java
@@ -5,14 +5,6 @@ import com.example.providerApi.dto.DinnerDTO;
 import com.example.providerApi.service.DinnerReadService;
 import com.example.providerApi.service.DinnerWriteService;
 import com.example.providerApi.util.Response;
-import io.github.swagger2markup.GroupBy;
-import io.github.swagger2markup.Language;
-import io.github.swagger2markup.Swagger2MarkupConfig;
-import io.github.swagger2markup.Swagger2MarkupConverter;
-import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
-import io.github.swagger2markup.markup.builder.MarkupLanguage;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +16,6 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@Api("大吉大利，今晚吃鸡")
 @RequestMapping("/api/dinner")
 public class Dinners {
 
@@ -35,11 +26,9 @@ public class Dinners {
     private DinnerReadService dinnerReadService;
 
     @GetMapping("{id}")
-    @ApiOperation(value = "根据ID查找")
     public DinnerDTO find(@PathVariable Long id) throws Exception {
         Response<DinnerDTO> resp = dinnerReadService.findById(id);
         if (!resp.isSuccess()) {
-            log.error("find dinner by id:{} fail, cause:{}", id, resp.getError());
             //TODO: 约定统一的Exception
             throw new Exception(resp.getError());
         }
@@ -47,7 +36,6 @@ public class Dinners {
     }
 
     @GetMapping
-    @ApiOperation(value = "批量查找")
     public List<DinnerDTO> findByIds(List<Long> ids) throws Exception {
         Response<List<DinnerDTO>> resp = dinnerReadService.findByIds(ids);
         if (!resp.isSuccess()) {
@@ -59,7 +47,6 @@ public class Dinners {
     }
 
     @PostMapping
-    @ApiOperation(value = "创建&更新")
     public Boolean save(@RequestBody DinnerDTO dinnerDTO) throws Exception {
         Response<Boolean> response = dinnerWriteService.save(dinnerDTO);
         if (!response.isSuccess()) {
@@ -70,7 +57,6 @@ public class Dinners {
     }
 
     @DeleteMapping
-    @ApiOperation(value = "删除")
     public Boolean deleteById(Long id) throws Exception {
         Response<Boolean> resp = dinnerWriteService.deleteById(id);
         if (!resp.isSuccess()) {
@@ -80,19 +66,19 @@ public class Dinners {
         return resp.getResult();
     }
 
-    @GetMapping("/swagger")
-    public void swagger() throws MalformedURLException {
-        Path outputFile = Paths.get("build/swagger");
-        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
-                .withMarkupLanguage(MarkupLanguage.MARKDOWN)
-                .withOutputLanguage(Language.ZH)
-                .withPathsGroupedBy(GroupBy.TAGS)
-                .withGeneratedExamples()
-                .withoutInlineSchema()
-                .build();
-        Swagger2MarkupConverter converter = Swagger2MarkupConverter.from(new URL("http://localhost:8000/v2/api-docs"))
-                .withConfig(config)
-                .build();
-        converter.toFile(outputFile);
-    }
+//    @GetMapping("/swagger")
+//    public void swagger() throws MalformedURLException {
+//        Path outputFile = Paths.get("build/swagger");
+//        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+//                .withMarkupLanguage(MarkupLanguage.MARKDOWN)
+//                .withOutputLanguage(Language.ZH)
+//                .withPathsGroupedBy(GroupBy.TAGS)
+//                .withGeneratedExamples()
+//                .withoutInlineSchema()
+//                .build();
+//        Swagger2MarkupConverter converter = Swagger2MarkupConverter.from(new URL("http://localhost:8000/v2/api-docs"))
+//                .withConfig(config)
+//                .build();
+//        converter.toFile(outputFile);
+//    }
 }
--- provider-api/src/main/java/com/example/providerApi/dto/DinnerDTO.java
+++ provider-api/src/main/java/com/example/providerApi/dto/DinnerDTO.java
@@ -1,45 +1,33 @@
 package com.example.providerApi.dto;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+
 import lombok.Data;
 
 import java.io.Serializable;
 import java.util.Date;
 
 @Data
-@ApiModel("点餐")
 public class DinnerDTO implements Serializable {
 
     private static final long serialVersionUID = -8671238782233084826L;
 
-    @ApiModelProperty("点餐的ID")
     private Long id;
 
-    @ApiModelProperty("点餐人姓名")
     private String userName;
 
-    @ApiModelProperty("点餐数量")
     private Integer number;
 
-    @ApiModelProperty("点餐状态。 1：吃鸡 0：b不吃")
     private Integer status;
 
-    @ApiModelProperty("年")
     private Integer year;
 
-    @ApiModelProperty("月")
     private Integer month;
 
-    @ApiModelProperty("日")
     private Integer day;
 
-    @ApiModelProperty("周几， 0,1,2,3,4,5,6")
     private Integer week;
 
-    @ApiModelProperty("创建时间")
     private Date createDate;
 
-    @ApiModelProperty("最后更新时间")
     private Date updateDate;
 }
